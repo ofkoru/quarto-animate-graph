2344d08ebecb5742d054357864b94533c92aa7ba
Koru <oqk5084@psu.edu>
2024-05-10 13:07:58 -0400

    First commit

---
+++ _extensions/animate-graph/animate-graph.js
@@ -0,0 +1,16 @@
+window.RevealAnimategraph = function () {
+  return {
+    id: "RevealAnimategraph",
+    init: function (deck) {
+      // TODO: Implement your plugin functionality
+      // Learn more at https://revealjs.com/creating-plugins/
+      
+      // This example shuffles the deck when the 'T' key is pressed
+      deck.addKeyBinding({ keyCode: 84, key: "T" }, () => {
+        deck.shuffle();
+      });
+
+    },
+  };
+};
+
